d115dfc809a7243dea8855bd64de5e038e144602
HoaNS-Picon <hoa.ns@picontechnology.com>
2026-02-19 15:05:08 +0700

    remove old src code

--- src/apps/chatbot/common/state.py
+++
@@ -1,8 +0,0 @@
-from langchain_core.documents import Document
-from typing_extensions import TypedDict, List
-
-
-class State(TypedDict):
-    question: str
-    context: List[Document]
-    answer: str
--- src/main.py
+++
@@ -1,7 +0,0 @@
-import uvicorn
-
-import config
-
-if __name__ == "__main__":
-    uvicorn.run("api.main:app", host="127.0.0.1", port=config.API_PORT, reload=True)
-
